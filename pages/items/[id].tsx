4b9b23cb64b9e0a508cde0ebcd913fec1e2b74be
CHOI HYUNWOO <kr.choi.hyunwoo@gmail.com>
2022-07-19 13:52:36 +0900

    Streams

--- pages/items/[id].tsx
+++ pages/items/[id].tsx
@@ -16,7 +16,7 @@ const ItemDetail: NextPage = () => {
         </div>
         <div className="mt-5">
           <h1 className="text-3xl font-bold text-gray-900">Galaxy S50</h1>
-          <span className="mt-3 block text-3xl text-gray-900">$140</span>
+          <span className="mt-3 block text-2xl text-gray-900">$140</span>
           <p className=" my-6 text-gray-700">
             My money&apos;s in that office, right? If she start giving me some
             bullshit about it ain&apos;t there, and we got to go someplace else
